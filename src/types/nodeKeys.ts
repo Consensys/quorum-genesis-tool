92e6334042f2dcc12763d6f76dd73d10e265e5f2
Joshua Fernandes <joshua.fernandes@consensys.net>
2022-01-27 21:39:51 +1000

    node key working and tests good

---
+++ src/types/nodeKeys.ts
@@ -0,0 +1,22 @@
+
+
+export type PrivateKey = Buffer;
+
+export type PublicKey = Buffer;
+
+export type Address = Buffer;
+
+export type EthAccount = {
+  privateKey: string;
+  publicKey: string;
+  address: string;
+  keystore: string;
+  password: string;
+}
+
+export type NodeKeys = {
+  privateKey: PrivateKey;
+  publicKey: PublicKey;
+  address: Address;
+  ethAccount : EthAccount
+};
