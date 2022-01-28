80ac6290c7b4e8c70c7d4cb8f7aa8abb5d424a23
Eric Lin <eric.lin@consensys.net>
2022-01-28 12:05:31 +1000

    add all questions
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -45,72 +45,78 @@ const _outputDirQuestion: QuestionTree = {
   }
 };
 
+const _outputUserInputs: QuestionTree = {
+  name: "userinput",
+  prompt: "Export your answers to file? Default: y",
+};
+_outputUserInputs.transformerValidator = _getYesNoValidator(_outputUserInputs, _outputDirQuestion, "y");
+
 const _tesseraQuestion: QuestionTree = {
   name: "tessera",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Generate tessera config file? Default: y",
 };
-_tesseraQuestion.transformerValidator = _getYesNoValidator(_tesseraQuestion, _outputDirQuestion);
+_tesseraQuestion.transformerValidator = _getYesNoValidator(_tesseraQuestion, _outputUserInputs, "y");
 
 const _permissionQuestion: QuestionTree = {
   name: "permissions",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Generate permissions config file? Default: y",
 };
-_permissionQuestion.transformerValidator = _getYesNoValidator(_permissionQuestion, _tesseraQuestion);
+_permissionQuestion.transformerValidator = _getYesNoValidator(_permissionQuestion, _tesseraQuestion, "y");
 
 const _staticNodesQuestion: QuestionTree = {
   name: "staticnodes",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Generate static node config file? Default: y",
 };
-_staticNodesQuestion.transformerValidator = _getYesNoValidator(_staticNodesQuestion, _permissionQuestion);
+_staticNodesQuestion.transformerValidator = _getYesNoValidator(_staticNodesQuestion, _permissionQuestion, "y");
 
 const _curveQuestion: QuestionTree = {
   name: "curve",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Choose your encryption curve: Default: [1]",
   options: [
-    { label: "secp256k1", value: "k1", nextQuestion: _staticNodesQuestion },
+    { label: "secp256k1", value: "k1", nextQuestion: _staticNodesQuestion, default: true },
     { label: "secp256r1", value: "r1", nextQuestion: _staticNodesQuestion }
   ]
 };
 
 const _bootnodesQuestion: QuestionTree = {
   name: "bootnodes",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Choose number of bootnode node keys to generate: (integer) Default: 2",
 };
-_bootnodesQuestion.transformerValidator = _integerValidator(_bootnodesQuestion, _curveQuestion);
+_bootnodesQuestion.transformerValidator = _integerValidator(_bootnodesQuestion, _curveQuestion, 2);
 
 const _membersQuestion: QuestionTree = {
   name: "members",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Choose number of member node keys to generate: (integer)",
 };
-_membersQuestion.transformerValidator = _integerValidator(_membersQuestion, _bootnodesQuestion);
+_membersQuestion.transformerValidator = _integerValidator(_membersQuestion, _bootnodesQuestion, 1);
 
 const _validatorsQuestion: QuestionTree = {
   name: "validators",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Choose number of validator node keys to generate: (integer) Default: 4",
 };
-_validatorsQuestion.transformerValidator = _integerValidator(_validatorsQuestion, _membersQuestion);
+_validatorsQuestion.transformerValidator = _integerValidator(_validatorsQuestion, _membersQuestion, 4);
 
 const _txnSizeLimitQuestion: QuestionTree = {
   name: "txnsize",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Set your transaction size limit value: (integer)",
 };
 _txnSizeLimitQuestion.transformerValidator = _integerValidator(_txnSizeLimitQuestion, _validatorsQuestion);
 
 const _maxCodeSizeQuestion: QuestionTree = {
   name: "maxsize",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Set your max code size value: (integer)",
 };
 _maxCodeSizeQuestion.transformerValidator = _integerValidator(_maxCodeSizeQuestion, _txnSizeLimitQuestion);
 
 const _coinbaseQuestion: QuestionTree = {
   name: "coinbase",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Set your coinbase address for rewards: (hex)",
 };
 _coinbaseQuestion.transformerValidator = _integerValidator(_coinbaseQuestion, _maxCodeSizeQuestion);
 
 const _gasLimitQuestion: QuestionTree = {
   name: "gaslimit",
-  prompt: "Do you wish to enable support for logging with Splunk or ELK (Elasticsearch, Logstash & Kibana)? Default: [1]",
+  prompt: "Set your gas limit value: (integer)",
 };
 _gasLimitQuestion.transformerValidator = _integerValidator(_gasLimitQuestion, _coinbaseQuestion);
 
