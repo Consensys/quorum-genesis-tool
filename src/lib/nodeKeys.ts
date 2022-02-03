0f214389eaf7e4eb7ba1c50ccb4136c0138e12d4
Eric Lin <eric.lin@consensys.net>
2022-02-03 13:02:04 +1000

    add ibft2 extradata
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- src/lib/nodeKeys.ts
+++ src/lib/nodeKeys.ts
@@ -12,34 +12,34 @@ import { Consensus } from "../types/consensus";
 const VANITY_DATA = "0x0000000000000000000000000000000000000000000000000000000000000000";
 const CLIQUE_PROPOSER_SIGNATURE = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
 
-export function generatePrivateKey() : PrivateKey {
-  let privKey : PrivateKey;
+export function generatePrivateKey(): PrivateKey {
+  let privKey: PrivateKey;
   do {
     privKey = randomBytes(32);
-  } while ( !secp256k1.privateKeyVerify(privKey) );
+  } while (!secp256k1.privateKeyVerify(privKey));
   return privKey;
 }
 
 export function derivePublicKey(privKey: Buffer): PublicKey {
   // slice on the end to remove the compression prefix ie. uncompressed use 04 prefix & compressed use 02 or 03
   // we generate the address, which wont work with the compression prefix
-  const pubKey : Uint8Array = secp256k1.publicKeyCreate(privKey, false).slice(1);
+  const pubKey: Uint8Array = secp256k1.publicKeyCreate(privKey, false).slice(1);
   return Buffer.from(pubKey);
 }
 
-export function deriveAddress(pubKey: Uint8Array) : Address  {
-  if(!Buffer.isBuffer(pubKey)) {
+export function deriveAddress(pubKey: Uint8Array): Address {
+  if (!Buffer.isBuffer(pubKey)) {
     console.error("ERROR - pubKey is not a buffer");
     process.exit();
   }
-  const keyHash : Address = keccak('keccak256').update(pubKey).digest();
+  const keyHash: Address = keccak('keccak256').update(pubKey).digest();
   return keyHash.slice(Math.max(keyHash.length - 20, 1));
 }
 
-export async function generateEthAccount(password: string) : Promise<EthAccount> {
-  const wallet : Wallet = Wallet.generate();
+export async function generateEthAccount(password: string): Promise<EthAccount> {
+  const wallet: Wallet = Wallet.generate();
   const v3keystore = await wallet.toV3(password);
-  const ethAccount : EthAccount = {
+  const ethAccount: EthAccount = {
     privateKey: wallet.getPrivateKeyString(),
     publicKey: wallet.getPublicKeyString(),
     address: wallet.getAddressString(),
@@ -49,12 +49,12 @@ export async function generateEthAccount(password: string) : Promise<EthAccount>
   return ethAccount;
 }
 
-export async function generateNodeKeys(password: string) : Promise<NodeKeys> {
-  const privateKey : PrivateKey = generatePrivateKey();
-  const publicKey : PublicKey = derivePublicKey(privateKey);
-  const address : Address = deriveAddress(publicKey);
-  const ethAccount : EthAccount = await generateEthAccount(password);
-  const nodeKeys : NodeKeys = {
+export async function generateNodeKeys(password: string): Promise<NodeKeys> {
+  const privateKey: PrivateKey = generatePrivateKey();
+  const publicKey: PublicKey = derivePublicKey(privateKey);
+  const address: Address = deriveAddress(publicKey);
+  const ethAccount: EthAccount = await generateEthAccount(password);
+  const nodeKeys: NodeKeys = {
     privateKey,
     publicKey,
     address,
@@ -65,15 +65,15 @@ export async function generateNodeKeys(password: string) : Promise<NodeKeys> {
 
 // extraData field calculation
 
-export function generateExtraDataString(validatorAddressList: Address[], consensus: Consensus) : string {
+export function generateExtraDataString(validatorAddressList: Address[], consensus: Consensus): string {
 
-  let extraData : Input;
-  switch(consensus) {
+  let extraData: Input;
+  switch (consensus) {
     case Consensus.clique: {
       // clique just appends https://besu.hyperledger.org/en/latest/HowTo/Configure/Consensus-Protocols/Clique/
       // 65 bytes for the proposer signature. In the genesis block there is no initial proposer, so the proposer signature is all zeros.
       // [0x prefix, 32 bytes vanity, List<Validators> (20bytes each), proposer signature - 65 bytes of 0s (see comment above)]
-      const signers : string[] = validatorAddressList.map(_ => _.toString('hex'));
+      const signers: string[] = validatorAddressList.map(_ => _.toString('hex'));
       extraData = [VANITY_DATA, signers.join(''), CLIQUE_PROPOSER_SIGNATURE].join('');
       break;
     }
@@ -82,31 +82,32 @@ export function generateExtraDataString(validatorAddressList: Address[], consens
       extraData = VANITY_DATA;
       break;
     }
-    // ibft 
+    // ibft
     case Consensus.ibft: {
       // ibft = 0x + <32 bytes Vanity> + RLP(List<Validators>, proposer Seals, seals]).
-      const extraDataContent : Input = [validatorAddressList, null, []];
-      const extraDataCoded : Uint8Array = RLP.encode(extraDataContent);
+      const extraDataContent: Input = [validatorAddressList, null, []];
+      const extraDataCoded: Uint8Array = RLP.encode(extraDataContent);
       extraData = VANITY_DATA + Buffer.from(extraDataCoded).toString('hex');
       break;
     }
     // ibft2
-    Consensus.ibft2: {
-      // ibft2, qbft as normal ie RLP([32 bytes Vanity, List<Validators>, No Vote, Round=Int(0), 0 Seals]).
-      const extraDataContent : Input = [new Uint8Array(32), validatorAddressList, [], null, []];
-      const extraDataCoded : Uint8Array = RLP.encode(extraDataContent);
+    case Consensus.ibft2: {
+      const roundBuffer: Buffer = Buffer.alloc(4);
+      roundBuffer.writeUInt32LE(0, 0);
+      const extraDataContent: Input = [new Uint8Array(32), validatorAddressList, null, roundBuffer, []];
+      const extraDataCoded: Uint8Array = RLP.encode(extraDataContent);
       extraData = '0x' + Buffer.from(extraDataCoded).toString('hex');
       break;
     }
     // qbft
     default: {
       // ibft2, qbft as normal ie RLP([32 bytes Vanity, List<Validators>, No Vote, Round=Int(0), 0 Seals]).
-      const extraDataContent : Input = [new Uint8Array(32), validatorAddressList, [], null, []];
-      const extraDataCoded : Uint8Array = RLP.encode(extraDataContent);
+      const extraDataContent: Input = [new Uint8Array(32), validatorAddressList, [], null, []];
+      const extraDataCoded: Uint8Array = RLP.encode(extraDataContent);
       extraData = '0x' + Buffer.from(extraDataCoded).toString('hex');
       break;
     }
- }
- return extraData;
+  }
+  return extraData;
 
 }
