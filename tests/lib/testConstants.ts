4c2c19183e9758489d1e30138af754ef68e6c36a
Eric Lin <eric.lin@consensys.net>
2022-05-04 11:32:00 +1000

    add quickstartDevAccounts to tests
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- tests/lib/testConstants.ts
+++ tests/lib/testConstants.ts
@@ -1,4 +1,3 @@
-
 import { Consensus } from "../../src/types/consensus";
 import { QuorumConfig } from "../../src/types/quorumConfig";
 import { CryptoCurve } from "../../src/types/cryptoCurve";
@@ -21,13 +20,16 @@ export const TEST_QUORUM_CONFIG: QuorumConfig = {
   curve: CryptoCurve.k1,
   outputPath: "./output",
   tesseraEnabled: false,
-  tesseraPassword: ""
+  tesseraPassword: "",
+  quickstartDevAccounts: false,
 };
 
 export const TEST_NODE = {
-  privateKey: "865ea77fe933ba01ad4ff60b7c9187ff34be5327350c5045153e5cfa85302013",
-  publicKey: "beed1b5bc414a2cc13d04d6017f9ebeec9818ba1b8c97be5776328490c69c5f0a07edc20589fa373c99aeb199d685ea177b93a4c8eb57f91304883b3d9c449ff",
+  privateKey:
+    "865ea77fe933ba01ad4ff60b7c9187ff34be5327350c5045153e5cfa85302013",
+  publicKey:
+    "beed1b5bc414a2cc13d04d6017f9ebeec9818ba1b8c97be5776328490c69c5f0a07edc20589fa373c99aeb199d685ea177b93a4c8eb57f91304883b3d9c449ff",
   address: "3db22f82cab50038f33e33cb35a86c92f5bc18f4",
   password: "",
-  curve: CryptoCurve.k1
+  curve: CryptoCurve.k1,
 };
